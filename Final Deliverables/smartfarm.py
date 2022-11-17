e368ddeef61d6ff2bd9c1d8e3195305b4ea0bec4
M.DILIP KUMAR <113820501+HunterDilip@users.noreply.github.com>
2022-11-18 00:00:51 +0530

    Add files via upload

---
+++ Final Deliverables/smartfarm.py
@@ -0,0 +1,38 @@
+#IBM Watson IOT Platform
+#pip install wiotp-sdk
+import wiotp.sdk.device
+import time
+import random
+myConfig = { 
+    "identity": {
+        "orgId": "3podkf",
+        "typeId": "smartfarm",
+        "deviceId":"PNT2022TMID15139"
+    },
+    "auth": {
+        "token": "z(TNhcKsgpIbASn-8K"
+    }
+}
+
+def myCommandCallback(cmd):
+    print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
+    m=cmd.data['command']
+    if(m=="lighton"):
+        print("*****///LIGHTS ARE ON////*****")
+    elif(m=="lightoff"):
+        print("*****///LIGHTS ARE OFF////*****")
+    else:
+        print("*****///WRONG COMMAND/////*****")
+
+client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
+client.connect()
+
+while True:
+    temp=random.randint(-20,125)
+    hum=random.randint(0,100)
+    myData={'temperature':temp, 'humidity':hum}
+    client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
+    print("Published data Successfully: %s", myData)
+    client.commandCallback = myCommandCallback
+    time.sleep(2)
+client.disconnect()
